3a895f015cc8b1b8f449866dc050fb5b7ba7d420
replica <60418809+junobonnie@users.noreply.github.com>
2023-07-25 22:31:01 +0900

    Add files via upload

---
+++ vectortools_np.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 21:17:58 2023
+
+@author: replica
+"""
+import math as m
+import numpy as np
+
+class Tensor:
+    def __init__(self, xx, xy, yx, yy):
+        self.xx = xx
+        self.xy = xy
+        self.yx = yx
+        self.yy = yy
+        
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return Tensor(-self.xx, -self.xy, -self.yx, -self.yy)
+        
+    def __abs__(self):
+        return self.xx*self.yy - self.xy*self.yx
+    
+    def __add__(self, other_Tensor):
+        return Tensor(self.xx + other_Tensor.xx, self.xy + other_Tensor.xy,
+                      self.yx + other_Tensor.yx, self.yy + other_Tensor.yy)
+    
+    def __sub__(self, other_Tensor):
+        return Tensor(self.xx - other_Tensor.xx, self.xy - other_Tensor.xy,
+                      self.yx - other_Tensor.yx, self.yy - other_Tensor.yy)
+    
+    def __mul__(self, num):
+        return Tensor(self.xx*num, self.xy*num, self.yx*num, self.yy*num)
+    
+    def __rmul__(self, num):
+        return Tensor(self.xx*num, self.xy*num, self.yx*num, self.yy*num)
+    
+    def __truediv__(self, num):
+        return Tensor(self.xx/num, self.xy/num, self.yx/num, self.yy/num)
+    
+    def __pow__(self, num):
+        result = Tensor(1, 0, 0, 1)
+        if num < 0:
+            pow_tensor = self.inverse()
+        else:
+            pow_tensor = self
+        for i in range(abs(num)):
+            result = result.dot(pow_tensor)
+        return result
+    
+    def __eq__(self, other):
+        if str(self) == str(other):
+            return True
+        else:
+            return False
+        
+    def __ne__(self, other):
+        if str(self) != str(other):
+            return True
+        else:
+            return False
+        
+    def __str__(self):
+        return 'Tensor(' + str(self.xx) + ', ' + str(self.xy) + ', ' + str(self.yx) + ', ' + str(self.yy) + ')'
+    
+    def inverse(self):
+        if abs(self) == 0:
+            return None
+        else:
+            return Tensor(self.yy, -self.xy, -self.yx, self.xx)/abs(self)
+        
+    def T(self):
+        return Tensor(self.xx, self.yx, self.xy, self.yy)
+        
+    def dot(self, other_object):
+        if isinstance(other_object, Tensor):
+            return Tensor(self.xx*other_object.xx + self.xy*other_object.yx, self.xx*other_object.xy + self.xy*other_object.yy, 
+                          self.yx*other_object.xx + self.yy*other_object.yx, self.yx*other_object.xy + self.yy*other_object.yy)
+            
+        elif isinstance(other_object, Vector):
+            return Vector(self.xx*other_object.x + self.xy*other_object.y, self.yx*other_object.x + self.yy*other_object.y)
+        
+    def tuple(self):
+        return ((self.xx, self.xy),
+                (self.yx, self.yy))
+    
+    def list(self):
+        return [[self.xx, self.xy],
+                [self.yx, self.yy]]
+    
+    def array(self):
+        return np.array([[self.xx, self.xy],
+                         [self.yx, self.yy]])
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.arr = self.array()
+     
+    def __pos__(self):
+        return self
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+        
+    def __abs__(self):
+        return np.linalg.norm(self.arr)
+    
+    def __add__(self, other_Vector):
+        new_arr = self.arr + other_Vector.arr
+        return Vector(new_arr[0], new_arr[1])
+    
+    def __sub__(self, other_Vector):
+        new_arr = self.arr - other_Vector.arr
+        return Vector(new_arr[0], new_arr[1])
+    
+    def __mul__(self, num):
+        new_arr = num*self.arr
+        return Vector(new_arr[0], new_arr[1])
+    
+    def __rmul__(self, num):   
+        new_arr = self.arr*num
+        return Vector(new_arr[0], new_arr[1])
+    
+    def __truediv__(self, num):
+        new_arr = self.arr/num
+        return Vector(new_arr[0], new_arr[1])
+    
+    def __eq__(self, other):
+        if (self.arr == other.arr).all():
+            return True
+        else:
+            return False
+        
+    def __ne__(self, other):
+        if (self.arr != other.arr).all():
+            return True
+        else:
+            return False
+    
+    def __str__(self):
+        return 'Vector(' + str(self.x) + ', ' + str(self.y) + ')'
+    
+    def dot(self, other_Vector):
+        return np.dot(self.arr, other_Vector.arr)
+    
+    def cross(self, other_Vector):
+        return np.cross(self.arr, other_Vector.arr)
+    
+    def tuple(self):
+        return (self.x, self.y)
+    
+    def list(self):
+        return [self.x, self.y]
+    
+    def array(self):
+        return np.array([self.x, self.y])
+    
+def SO2(theta):
+    return Tensor(m.cos(theta), -m.sin(theta), m.sin(theta), m.cos(theta))
+
+if __name__ == '__main__':
+    t1 = Tensor(1, 2,
+                3, 4)
+    t2 = SO2(m.pi/4)
+    t3 = Tensor(1, 2,
+                3, 4)
+    print(abs(t1))
+    print(t1 + t2)
+    print(t1 - t2)
+    print(t1*2)
+    print(2*t1)
+    print(t1/2)
+    print(t1**3)
+    print(t1**(-1))
+    print(t1 == t3)
+    print(t1 != t3)
+    print(t1)
+    print(t1.inverse())
+    print(t1.T())
+    print(t1.dot(t2))
+    print(t1.tuple())
+    print(t1.list())
+    print(t1.array())
+    
+    v1 = Vector(3, 4)
+    v2 = Vector(4, 3)
+    print(t1.dot(v1))
+    print(abs(v1))
+    print(v1 + v2)
+    print(v1 - v2)
+    print(2*v1)
+    print(v1*2)
+    print(v1/2)
+    print(v1 == v2)
+    print(v1 != v2)
+    print(v1)
+    print(v1.dot(v2))
+    print(v1.cross(v2))
+    print(v1.tuple())
+    print(v1.list())
+    print(v1.array())
